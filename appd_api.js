cd994c52b9af3aaaf2a3cbf3da4147bccae6180d
David Winterbourne <david.winterbourne@appdynamics.com>
2016-06-23 13:03:07 -0700

    Wrapped all API calls for AppD api

--- appd_api.js
+++ appd_api.js
@@ -1,15 +1,78 @@
+var DEFAULT_APP = "ECommerce-E2E";
+
 $(function(){
 
-	$.ajax
-	  ({
-	    type: "GET",
-	    url: "http://127.0.0.1:3000/controller/rest/applications/ECommerce-E2E/nodes?output=json",
-	    success: function (response) {
-	    	console.log("response: " + JSON.stringify(response)); 
-	    },
-	    failure: function (response) {
-	    	console.log("Failed to load controller request: " + JSON.stringify(response))
-	    }
-	});
+	function appdApiCall(endPoint, onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		$.ajax
+		  ({
+		    type: "GET",
+		    url: "http://127.0.0.1:3000/controller/rest/applications/" + applicationName + "/" + endPoint + "?output=json",
+		    success: function (response) {
+		    	console.log("response: " + JSON.stringify(response)); 
+		    	onSuccess(response);
+		    },
+		    failure: function (response) {
+		    	console.log("Failed to load controller request: " + JSON.stringify(response))
+		    	if (onFailure != null) {
+		    		onFailure(response);
+		    	}
+		    }
+		});
+	}
+
+	function loadBizTxn(onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("business-transactions", onSuccess, onFailure, applicationName)
+	}
+
+	function loadTiers(onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("tiers", onSuccess, onFailure, applicationName)
+	}
+
+	function loadBackends(onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("backends", onSuccess, onFailure, applicationName)
+	}
+
+	function loadNodes(onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("nodes", onSuccess, onFailure, applicationName)
+	}
+
+	function loadNodeInfo(nodeName, onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("nodes/" + nodeName, onSuccess, onFailure, applicationName)
+	}
+
+	function loadNodesInTier(tierName, onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("tiers/" + tierName + "/nodes", onSuccess, onFailure, applicationName)
+	}
+
+	function loadTierInfo(tierName, onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("tiers/" + tierName, onSuccess, onFailure, applicationName)
+	}
+
+	function loadMetricHierarchy(onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		appdApiCall("metrics", onSuccess, onFailure, applicationName)
+	}
+
+    /*
+    http://ec2-54-213-152-245.us-west-2.compute.amazonaws.com:8090
+    /controller/rest/applications/ECommerce-E2E/metric-data?
+    metric-path=Overall%20Application%20Performance%7CECommerce-Services%7CIndividual%20Nodes%7CECommerce-E2E_WEB1%7CCalls%20per%20Minute &
+    time-range-type=BEFORE_NOW &
+    duration-in-mins=240
+
+    See: https://docs.appdynamics.com/display/PRO42/Metric+and+Snapshot+API#MetricandSnapshotAPI-usingtimeranges
+    */
+	function loadMetricData(metricPath, timeRangeType, durationInMins, startTime, endTime, onSuccess, onFailure, applicationName = DEFAULT_APP) {
+		var metricDataParams = metricPath + "&" + timeRangeType;
+		if (durationInMins != null) {
+			metricDataParams +=  "&" + durationInMins
+		}
+		if (startTime != null) {
+			metricDataParams +=  "&" + startTime	
+		}
+		if (endTime != null) {
+			metricDataParams +=  "&" + endTime	
+		}
+		appdApiCall("metric-data?" + encodeURI(metricDataParams), onSuccess, onFailure, applicationName)
+	}	
 
 });
