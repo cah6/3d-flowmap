81d106e83ab76a298fe548a785f7449aca98e8cc
Justin Posey <justin.posey@appdynamics.com>
2016-06-23 15:52:55 -0500

    First hack at controls

--- 3d_flowmap.js
+++ 3d_flowmap.js
@@ -1,20 +1,20 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 150;
 camera.lookAt(0, 0, 0);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // add light behind camera
 var light1 = new THREE.PointLight(0xffffff);
-light1.position.set(0,0,1000);
+light1.position.set(0, 0, 1000);
 scene.add(light1);
 
 // and way in front
 var light2 = new THREE.PointLight(0xffffff);
-light2.position.set(0,0,-1000);
+light2.position.set(0, 0, -1000);
 scene.add(light2);
 
 var clock = new THREE.Clock();
@@ -45,8 +45,8 @@ function render() {
     var delta = clock.getDelta();
     camControls.update(delta);
     //updateParticles(particleData);
-
-    renderer.render( scene, camera );
+    pointerLock();
+    renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
 
@@ -84,15 +84,15 @@ function updateParticles(pData) {
 }
 
 function createNode(name, x, y, z, size) {
-    var radius = size/2;
+    var radius = size / 2;
     var geometry = new THREE.SphereGeometry(radius, 10, 10);
     var material = new THREE.MeshLambertMaterial();
-    var sphere = new THREE.Mesh( geometry, material );
+    var sphere = new THREE.Mesh(geometry, material);
     sphere.position.set(x, y, z);
     scene.add(sphere);
 
     // give it a floating label
-    createNameLabel(name, x, y, z, size/2);
+    createNameLabel(name, x, y, z, size / 2);
 
     return sphere;
 }
@@ -108,7 +108,7 @@ function createDataflow(fromObject, toObject, callsPerMin) {
         toObject.getWorldPosition()
     );
 
-    var line = new THREE.Line( geometry, material );
+    var line = new THREE.Line(geometry, material);
     scene.add(line);
 
     // create the particle variables
@@ -165,10 +165,10 @@ function createNameLabel(text, x, y, z, size) {
         });
 
         var textMaterial = new THREE.MeshPhongMaterial(
-            { color: 0xdddddd, specular: 0xffffff }
+            {color: 0xdddddd, specular: 0xffffff}
         );
 
-        var textMesh = new THREE.Mesh( textGeometry, textMaterial );
+        var textMesh = new THREE.Mesh(textGeometry, textMaterial);
         // move it to the right, plus a buffer
         textMesh.position.set(x + size + 5, y - size + 5, z);
 
@@ -177,4 +177,191 @@ function createNameLabel(text, x, y, z, size) {
     });
 }
 
+function pointerLock() {
+    var controls;
+
+    var objects = [];
+
+    var raycaster;
+
+    var blocker = document.getElementById('blocker');
+    var instructions = document.getElementById('instructions');
+
+    var havePointerLock = 'pointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+    if (havePointerLock) {
+        var element = document.body;
+
+        var pointerlockchange = function (event) {
+            if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+                controlsEnabled = true;
+                controls.enabled = true;
+
+                blocker.style.display = 'none';
+            } else {
+                controls.enabled = false;
+
+                blocker.style.display = '-webkit-box';
+                blocker.style.display = '-moz-box';
+                blocker.style.display = 'box';
+
+                instructions.style.display = '';
+            }
+        };
+
+        var pointerlockerror = function (event) {
+            instructions.style.display = '';
+        };
+
+        // Hook pointer lock state change events
+        document.addEventListener('pointerlockchange', pointerlockchange, false);
+        document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+        document.addEventListener('pointerlockerror', pointerlockerror, false);
+        document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+        instructions.addEventListener('click', function (event) {
+            instructions.style.display = 'none';
+
+            // Ask the browser to lock the pointer
+            element.requestPointerLock = element.requestPointerLock || element.webkitRequestPointerLock;
+
+            element.requestPointerLock();
+        }, false);
+    } else {
+        instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+    }
+
+    init();
+    animate();
+
+    var canJump = false;
+    var moveLeft = false;
+    var moveRight = false;
+    var moveForward = false;
+    var moveBackward = false;
+    var controlsEnabled = false;
+    var prevTime = performance.now();
+    var velocity = new THREE.Vector3();
+
+    function init() {
+        controls = new THREE.PointerLockControls(camera);
+        scene.add(controls.getObject());
+
+        var onKeyDown = function (event) {
+            switch (event.keyCode) {
+                case 38: // up
+                case 87: // w
+                    moveForward = true;
+                    break;
+
+                case 37: // left
+                case 65: // a
+                    moveLeft = true;
+                    break;
+
+                case 40: // down
+                case 83: // s
+                    moveBackward = true;
+                    break;
+
+                case 39: // right
+                case 68: // d
+                    moveRight = true;
+                    break;
+
+                case 32: // space
+                    if (canJump === true) velocity.y += 350;
+                    canJump = false;
+                    break;
+            }
+        };
+
+        var onKeyUp = function (event) {
+            switch (event.keyCode) {
+                case 38: // up
+                case 87: // w
+                    moveForward = false;
+                    break;
+
+                case 37: // left
+                case 65: // a
+                    moveLeft = false;
+                    break;
+
+                case 40: // down
+                case 83: // s
+                    moveBackward = false;
+                    break;
+
+                case 39: // right
+                case 68: // d
+                    moveRight = false;
+                    break;
+            }
+        };
+
+        document.addEventListener('keydown', onKeyDown, false);
+        document.addEventListener('keyup', onKeyUp, false);
+
+        raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0), 0, 10);
+
+        window.addEventListener('resize', onWindowResize, false);
+    }
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+
+        if (controlsEnabled) {
+            raycaster.ray.origin.copy(controls.getObject().position);
+            raycaster.ray.origin.y -= 10;
+
+            var intersections = raycaster.intersectObjects(objects);
+
+            var isOnObject = intersections.length > 0;
+
+            var time = performance.now();
+            var delta = ( time - prevTime ) / 1000;
+
+            velocity.x -= velocity.x * 10.0 * delta;
+            velocity.z -= velocity.z * 10.0 * delta;
+
+            velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+            if (moveForward) velocity.z -= 400.0 * delta;
+            if (moveBackward) velocity.z += 400.0 * delta;
+
+            if (moveLeft) velocity.x -= 400.0 * delta;
+            if (moveRight) velocity.x += 400.0 * delta;
+
+            if (isOnObject === true) {
+                velocity.y = Math.max(0, velocity.y);
+
+                canJump = true;
+            }
+
+            controls.getObject().translateX(velocity.x * delta);
+            controls.getObject().translateY(velocity.y * delta);
+            controls.getObject().translateZ(velocity.z * delta);
+
+            if (controls.getObject().position.y < 10) {
+                velocity.y = 0;
+                controls.getObject().position.y = 10;
+
+                canJump = true;
+            }
+
+            prevTime = time;
+        }
+    }
+}
+
 render();
+
