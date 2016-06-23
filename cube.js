243c756b7c2c3082d9ff7ac4e730a30abb6e0d6d
Christian Henry <christian.henry92@gmail.com>
2016-06-22 17:10:14 -0700

    Add node creation method, basic fps controls (will probably move to PointerLock later)

--- cube.js
+++ cube.js
@@ -1,29 +1,80 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 150;
+camera.lookAt(0, 0, 0);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// LIGHT
+var light = new THREE.PointLight(0xffffff);
+light.position.set(0,0,1000);
+scene.add(light);
 
-//var textMaterial = new THREE.MeshPhongMaterial({ color: 0xdddddd });
-//var textGeom = new THREE.TextGeometry( 'Node1', {
-//    font: "helvetiker" // Must be lowercase!
-//});
-//var textMesh = new THREE.Mesh( textGeom, textMaterial );
-//scene.add( textMesh );
+var clock = new THREE.Clock();
 
-camera.position.z = 5;
+createNode("Node1", -10, 30, 0);
+createNode("Node2", -10, 0, 0);
+createNode("Node3", -10, -30, 0);
+
+var camControls = new THREE.FirstPersonControls(camera);
+camControls.lookSpeed = 0.08;
+camControls.movementSpeed = 60;
+camControls.noFly = true;
+camControls.lookVertical = true;
+camControls.constrainVertical = true;
+camControls.verticalMin = 1.0;
+camControls.verticalMax = 2.0;
+camControls.lon = -150;
+camControls.lat = 120;
 
 function render() {
     requestAnimationFrame( render );
-    cube.rotation.x += 0.05;
-    cube.rotation.y += 0.05;
+    var delta = clock.getDelta();
+    camControls.update(delta);
     renderer.render( scene, camera );
+}
+
+function createNode(name, x, y, z) {
+    var cubeSize = 20;
+    var geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    var material = new THREE.MeshNormalMaterial();
+    var cube = new THREE.Mesh( geometry, material );
+    cube.position.set(x, y, z);
+    scene.add( cube );
+
+
+    // give it a floating label
+    var loader = new THREE.FontLoader();
+    loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+        var textGeometry = new THREE.TextGeometry( name, {
+
+            font: font,
+
+            size: 10,
+            height: 5,
+            curveSegments: 12,
+
+            bevelThickness: 1,
+            bevelSize: 1,
+            bevelEnabled: true
+
+        });
+
+        var textMaterial = new THREE.MeshPhongMaterial(
+            { color: 0xdddddd, specular: 0xffffff }
+        );
+
+        var text = new THREE.Mesh( textGeometry, textMaterial );
+        text.position.set(x + cubeSize/2 + 5, y - cubeSize/2 + 5, z)
+
+        scene.add( text );
+
+    });
+
+    //return cube;
 }
 
 render();
